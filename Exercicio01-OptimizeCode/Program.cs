fcaeaf1e2f3a049add4ed0cea3b40c93baf6e34e
x14081950 <x14081950@D28G82200716339>
2023-01-18 11:12:38 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio01-OptimizeCode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Teste
+{
+    // Main Method
+    static public void Main(String[] args)
+    {
+        Dictionary<char, int> result = new Dictionary<char, int>();
+        result = (Dictionary<char, int>) Exercise.GetCharacterCount("John Doe");
+        result.ToList().ForEach(x => Console.WriteLine("'" + x.Key + "': " + x.Value));
+    }
+}
+
+public class Exercise
+{
+    // TODO: fix this method - remove boxing & unboxing, and return correct result
+    public static object GetCharacterCount(string name)
+    {
+        string n = name.Replace(" ", string.Empty); //remove espaços entre palavras
+        Dictionary<char, int> result = new Dictionary<char, int>();
+        for (int i = 0; i < n.Length; i++)
+        {
+            var c = Char.ToLower(n[i]); 
+            if (result.ContainsKey(c)) 
+            {
+                result[c] += 1; //caso o caractere já esteja no Dictionary, soma 1 ao contador da mesma
+            }
+            else
+            {
+                result[c] = 1; //caso não encontre, adiciona o caractere e inicia o contador
+            }
+        }
+        return result;
+    }
+}
+
---
+++ Exercicio01-OptimizeCode/README.md
@@ -0,0 +1,14 @@
+﻿Exercício 01 - Contagem de Caracteres em uma string
+
+Escreva um método GetCharacterCount na classe Exercise que recebea uma string 'name' e faça o seguinte:
+
+Realize a contagem de quantas vezes um caractere único aparece na variável. Retorne um Dictionary com cada caractere único como Key e a contagem de quantas vezes esse mesmo caractere aparece como value. Ignore espaços e trate todos os caracteres com lower case.
+
+Exemplo: name = "Jonh Doe"
+output:
+'j': 1
+'o': 2
+'h': 1
+'n': 1
+'d': 1
+'e': 1
